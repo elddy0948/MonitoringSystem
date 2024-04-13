3973558957e8055d8e6bb916e813434be274c7ba
Joons <elddy0948@gmail.com>
2024-04-13 14:18:30 +0900

    refactor : folder structure and code

--- MonitoringLCD/MonitoringDrivers/Src/stm32f76xxx_gpio.c
+++ MonitoringLCD/MonitoringDrivers/Src/stm32f76xxx_gpio.c
@@ -12,8 +12,8 @@ void GPIO_Initialize(GPIO_Handle_t* pGPIOHandle)
 	if(pGPIOHandle->PinConfig.PinMode <= GPIO_MODE_ANALOG)
 	{
 		// Clear mode bits
-		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->PinConfig->PinNumber);
-		pGPIOHandle->pGPIOx->MODER |= (pGPIOHandle->PinConfig->PinMode << (2 * pGPIOHandle->PinConfig->PinNumber));
+		pGPIOHandle->pGPIOx->MODER &= ~(0x3 << pGPIOHandle->PinConfig.PinNumber);
+		pGPIOHandle->pGPIOx->MODER |= (pGPIOHandle->PinConfig.PinMode << (2 * pGPIOHandle->PinConfig.PinNumber));
 	}
 	else
 	{
@@ -21,28 +21,28 @@ void GPIO_Initialize(GPIO_Handle_t* pGPIOHandle)
 	}
 
 	// Output type
-	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->PinConfig->PinNumber);
-	pGPIOHandle->pGPIOx->OTYPER |= (pGPIOHandle->PinConfig->OutputType << pGPIOHandle->PinConfig->PinNumber);
+	pGPIOHandle->pGPIOx->OTYPER &= ~(0x1 << pGPIOHandle->PinConfig.PinNumber);
+	pGPIOHandle->pGPIOx->OTYPER |= (pGPIOHandle->PinConfig.OutputType << pGPIOHandle->PinConfig.PinNumber);
 
 	// Output speed
-	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->PinConfig->PinNumber);
-	pGPIOHandle->pGPIOx->OSPEEDR |= (pGPIOHandle->PinConfig->OutputSpeed << pGPIOHandle->PinConfig->PinNumber);
+	pGPIOHandle->pGPIOx->OSPEEDR &= ~(0x3 << pGPIOHandle->PinConfig.PinNumber);
+	pGPIOHandle->pGPIOx->OSPEEDR |= (pGPIOHandle->PinConfig.OutputSpeed << pGPIOHandle->PinConfig.PinNumber);
 
 	/* PullUp PullDown control */
-	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->PinConfig->PinNumber);
-	pGPIOHandle->pGPIOx->PUPDR |= (pGPIOHandle->PinConfig->PullUpPullDown << pGPIOHandle->PinConfig->PinNumber);
+	pGPIOHandle->pGPIOx->PUPDR &= ~(0x3 << pGPIOHandle->PinConfig.PinNumber);
+	pGPIOHandle->pGPIOx->PUPDR |= (pGPIOHandle->PinConfig.PullUpPullDown << pGPIOHandle->PinConfig.PinNumber);
 
 	/* Alternate functionality */
 	if(pGPIOHandle->PinConfig.PinMode == GPIO_MODE_ALTERNATEFUNCTION)
 	{
 		uint8_t afr_position, pin_position = 0;
 
-		afr_position = pGPIOHandle->PinConfig->PinNumber / 8;
-		pin_position = pGPIOHandle->PinConfig->PinNumber % 8;
+		afr_position = pGPIOHandle->PinConfig.PinNumber / 8;
+		pin_position = pGPIOHandle->PinConfig.PinNumber % 8;
 
 		// Clear position
 		pGPIOHandle->pGPIOx->AFR[afr_position] &= ~(0xF << (4 * pin_position));
-		pGPIOHandle->pGPIOx->AFR[afr_position] |= (pGPIOHandle->PinConfig->AlternateFunction << (4 * pin_position));
+		pGPIOHandle->pGPIOx->AFR[afr_position] |= (pGPIOHandle->PinConfig.AlternateFunction << (4 * pin_position));
 	}
 }
 
